6a51f489dc712410673d9f8db7ecbba882718cb8
Sameersingh001 <mrsameersingh001gmail.com>
2025-10-06 14:32:56 +0530

    Sameer Created Register Page And Landing Page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ApplicationForm from "./pages/Intern/ApplicationForm";
 import HomePage from "./pages/Landing/Home";
+import RegisterPage from "./Authentication/RegisterForm";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/apply" element={<ApplicationForm />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Routes>
     
     </Router>
